46e7f0d2b6fe67c51594c0ee9af04f7751991e17
Raj Shah <rajnshah2020@gmail.com>
2025-09-27 23:27:18 -0400

    rename files

--- ProfileBlazarApp/Program.cs
+++ ProfileBlazarApp/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using profile_app_blazar;
+using ProfileBlazarApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- ProfileBlazarApp/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ ProfileBlazarApp/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -1128,7 +1128,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5009,
+    interval: 5185,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -6137,7 +6137,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5009
+    delay: 5185
   };
 
   /**
