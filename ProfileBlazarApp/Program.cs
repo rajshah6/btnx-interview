5d6150ea97f7f138a6170fbc8bdb1bf2d21cafb8
Raj Shah <rajnshah2020@gmail.com>
2025-09-28 00:43:23 -0400

    calling APIs from Blazor & DI of HttpClient

--- ProfileBlazarApp/Program.cs
+++ ProfileBlazarApp/Program.cs
@@ -1,11 +1,18 @@
+using ProfileBlazarApp;
+using ProfileBlazarApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ProfileBlazarApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Change to your API's https dev URL
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5253/")
+});
+
+builder.Services.AddScoped<PersonApiClient>();
 
 await builder.Build().RunAsync();
