0a93c02eac7730d70ddd2f3adf9adad1a6020ac7
Raj Shah <rajnshah2020@gmail.com>
2025-09-27 23:43:09 -0400

    add Person model and PersonStore service for in-memory storage

---
+++ ProfileWebAPI/Models/Person.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProfileWebAPI.Models;
+
+public class Person
+{
+    public int Id { get; set; } = 0;
+    public string FirstName { get; set; } = "";
+
+    public string LastName { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public string? Phone { get; set; }
+}
